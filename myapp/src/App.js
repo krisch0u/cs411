a53846040772f7864cbf094f8ac6949b5e0a6b6b
trishajaggi <trishajaggi@gmail.com>
2022-11-14 17:38:57 -0500

    "basic view"

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Username: <input type="text" name="username" placeholder="Username" required></input>
+          <br></br>
+          Password: <input type="text" name="password" placeholder="Password" required></input>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
