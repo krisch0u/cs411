1c6b423bb3795ed16310bc344a3e40dcd503aa88
bdauria1 <103322615+bdauria1@users.noreply.github.com>
2022-12-03 11:40:12 -0500

    redid the backend to work

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
-
+var user_location = 'boston'
 var token = 'Bearer Txw-uhYHT15LsmLZyoLnCVWv5yxA2H1UmJ4jXU2MJCQdTrXyy5ZzxwUc2rXfXDY87ftTvMTjtJjk3VvEDtxD3FoRh04vwBiCTnahFOZ9e2SmZUummNFvvb1-1D1xY3Yx'
-        var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/'
-        var yelp_url = 'https://api.yelp.com/v3/businesses/search?term=restaurants&location=San+Francisco'
-        function clientCallback() {
-            console.log('made it to the client callback')
-        }
-        var requestobj = {
-            'url': cors_anywhere_url + yelp_url,
-            'method': 'GET',
-            headers : {'Authorization': token},
-            error: function (jqXHR, textstatus, errorThrown) {
-                console.log('AJAX error, jqXHR = ', jqXHR, 'textstatus = ', textstatus, 'errorThrown = ', errorThrown)
-            }
-        }
-        $.ajax(requestobj)
-        .done(function(response) {
-            console.log('typeof response = ', typeof response)
-            console.log('response = ', response)
-        })
+var cors_anywhere_url = 'https://cors-anywhere.herokuapp.com/'
+var yelp_url = 'https://api.yelp.com/v3/businesses/search'
+function clientCallback() {
+    console.log('made it to the client callback')
+}
+var requestobj = {
+    'url': cors_anywhere_url + yelp_url,
+    'method': 'GET',
+    'headers': { 'Authorization': token },
+    'data': { 'term': 'food', 'location': user_location },
+}
+$.ajax(requestobj)
+.done(function(response) {
+    //make it so its sorted by rating
+    var sorted = response.businesses.sort(function(a, b) {
+        return b.rating - a.rating
+    })
+    //sort it alphabetically
+    var sorteds = sorted.sort(function(a, b) {
+        return a.name.localeCompare(b.name)
+
+    })
+    console.log(sorteds)
+})
