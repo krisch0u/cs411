34f0e249cdf0a3ecd36d7dd8b4d09cda956cd5f0
trishajaggi <trishajaggi@gmail.com>
2022-12-07 09:48:53 -0500

    Alert top rated restaurant

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,56 +1,75 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef, useCallback } from 'react';
 import { gapi } from 'gapi-script';
 import {GoogleLogin, GoogleLogout} from 'react-google-login';
-// import { BEARER_TOKEN } from './hooks/config';
-// import axios from 'axios';
+import { API_BASE_URL, BEARER_TOKEN } from './hooks/config';
+import axios from 'axios';
+
 // reminder to install in terminal using command: npm install react-google-login gapi-script
 
-function App() {
+function App () {
   const [ profile, setProfile ] = useState([]);
-  // const [food, setFood] = useState(null);
-  // const [city, setCity] = useState(null);
-  // const [restaurants, setRest] = useState([]);
+  const [place, setPlaces]=useState([]);
+  const [location, setLocation] = useState(null);
+  const [posts,setPosts] = useState([]);
   const inputRef = useRef(null);
   const clientId = '920757342306-retlhm1jpfe3j3q00uj5l6l2lqokps15.apps.googleusercontent.com';
-  // const options = {
-  //   method: 'GET',
-  //   headers: {
-  //     accept: 'application/json',
-  //     Authorization: 'Bearer %s' % BEARER_TOKEN
-  //   }
-  // };
+  const config = {
+    headers: {
+      Authorization: BEARER_TOKEN
+    },
+    params: {
+      term: 'food',
+      location: 'Boston'
+    }
+  };
   
-  // fetch('https://cors-anywhere.herokuapp.com/corsdemo/https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20', options)
-  //   .then(response => response.json())
-  //   .then(response => console.log(response))
-  //   .catch(err => console.error(err));
-
-  // const getRandomWords = () => {
-  //   const options = {
-  //       method: 'GET',
-  //       url: 'https://cors-anywhere.herokuapp.com/corsdemo/https://api.yelp.com/v3/businesses/search?sort_by=best_match&limit=20',
-  //       Authorization: 'Bearer %s' % BEARER_TOKEN,
-  //       params: {term: food, location: city},
-  //   }
-
-  //   axios.request(options).then((response) => {
-  //       alert(response.data)
-  //       console.log(response.data)
-  //       setRest(response.data)
-
-  //   }).catch((error) => {
-  //       console.error(error)
-  //   })
-  //}
+  const fetchNearbyPlacesWithYelp = async () => {
+    return axios
+    .get(API_BASE_URL, config)
+    .then(place => {
+      // Get places’ names and images.
+      setPlaces(
+        place.data.businesses.map(x => ({
+          name: x.name,
+          image_url: x.image_url,
+      })),
+    );
+    alert(place.data.businesses[0].name)
+  })
+  .catch(error => {
+    alert(error);
+    console.log(error);
+  });
+};
 
   function handleClick() {
     console.log(inputRef.current.value);
+    setLocation(inputRef);
     inputRef.current.value = null;
+
   }
 
-//   useEffect(() => {
-//     if (food) getRandomWords()
-// }, [food]);
+  // const fetchItems = async () => {
+  //   const data = await axios
+  //     .get(
+  //       `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?location=usa`,
+  //       {
+  //         headers: {
+  //           Authorization: `Bearer ${BEARER_TOKEN}`,
+  //         },
+  //         params: {
+  //           term: 'restaurants',
+  //           location:'Boston'
+  //         },
+  //       },
+  //     )
+  //     .then(json => {
+  //       setPlaces({ items: json.data.businesses });
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  //   }
 
   useEffect(() => {
         const initClient = () => {
@@ -81,6 +100,16 @@ function App() {
                     <p>Name: {profile.name}</p>
                     <p>Email Address: {profile.email}</p>
                     <br />
+                    <input 
+                      ref={inputRef}
+                      type="text"
+                      id="city"
+                      name="city"
+                      placeholder='Ex. Boston'
+                    />
+                    <button onClick={handleClick}>Enter</button>
+                    <button onClick={fetchNearbyPlacesWithYelp}>Get</button>
+
                     <br />
                     <GoogleLogout clientId={clientId} buttonText="Log out" onLogoutSuccess={logOut} />
                 </div>
--- myapp/src/hooks/config.js
+++ myapp/src/hooks/config.js
@@ -1,8 +1,8 @@
 
-export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3';
+export const API_BASE_URL = 'https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search';
 // remove this token
 // NOTE: as long as this token is saved in the frontend, the application is not production ready.
 // The Baerer token must be used on a server whose inner workings are not exposed to the outside world
 // For simplicity and the sake of this tutorial, I'll just leave it here like this
 // But just bear in mind: NEVER EVER save a bearer token inside the frontend code
-export const BEARER_TOKEN = 'rUPP9uIzQKJ2rtr6S8Yf_rPsg6D4LjGMRDw8iutdvO88U9vBvANjV6aJSVI_PJoq-B2wZbGi6hyfjgcS56ZCYk0vCtQZl-uldCAdcyidUTcm-4inffjhvUeT2ZmLY3Yx';
+export const BEARER_TOKEN = 'Bearer rUPP9uIzQKJ2rtr6S8Yf_rPsg6D4LjGMRDw8iutdvO88U9vBvANjV6aJSVI_PJoq-B2wZbGi6hyfjgcS56ZCYk0vCtQZl-uldCAdcyidUTcm-4inffjhvUeT2ZmLY3Yx';
